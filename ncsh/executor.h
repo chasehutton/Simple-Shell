bf369801a2b21393c87d283f105139ad8f2d800f
Huhan64 <74927932+Huhan64@users.noreply.github.com>
2022-08-14 00:04:45 -0400

    Delete ncsh directory

--- ncsh/executor.h
+++
@@ -1,4 +0,0 @@
-#if !defined(EXECUTOR_H)
-#define EXECUTOR_H
-int execute(Tree *t);
-#endif
--- ncsh/globexp.h
+++
@@ -1,7 +0,0 @@
-#if !defined(GLOB_EXP_H)
-#define GLOB_EXP_H
-char **get_files();
-int match_glob(const char *, const char *);
-int is_glob(char *);
-void free_files(char **);
-#endif
